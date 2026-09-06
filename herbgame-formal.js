6f39ff563474770ee91b183149a8f9395bb95da3
ff41009tw52-rgb <ff41009tw52@gmail.com>
2026-09-06 19:54:26 +0800

    Point formal herb game loader to compact runtime

--- herbgame-formal.js
+++ herbgame-formal.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const manifest={"css":["herbgame-formal-css-01.b64"],"js":["herbgame-formal-js-01.b64","herbgame-formal-js-02.b64","herbgame-formal-js-03.b64","herbgame-formal-js-04.b64","herbgame-formal-js-05.b64","herbgame-formal-js-06.b64","herbgame-formal-js-07.b64","herbgame-formal-js-08.b64","herbgame-formal-js-09.b64","herbgame-formal-js-10.b64","herbgame-formal-js-11.b64","herbgame-formal-js-12.b64","herbgame-formal-js-13.b64","herbgame-formal-js-14.b64","herbgame-formal-js-15.b64","herbgame-formal-js-16.b64","herbgame-formal-js-17.b64","herbgame-formal-js-18.b64","herbgame-formal-js-19.b64","herbgame-formal-js-20.b64","herbgame-formal-js-21.b64","herbgame-formal-js-22.b64","herbgame-formal-js-23.b64","herbgame-formal-js-24.b64","herbgame-formal-js-25.b64","herbgame-formal-js-26.b64","herbgame-formal-js-27.b64","herbgame-formal-js-28.b64","herbgame-formal-js-29.b64","herbgame-formal-js-30.b64","herbgame-formal-js-31.b64","herbgame-formal-js-32.b64","herbgame-formal-js-33.b64"]};
+  const manifest={"css":["herbgame-formal-css-01.b64"],"js":["herbgame-formal-core-01.b64","herbgame-formal-core-02.b64"]};
   async function joinFiles(files){
     const parts=[];
     for(const f of files){ const r=await fetch(f,{cache:'no-store'}); if(!r.ok) throw new Error(`載入失敗: ${f}`); parts.push(await r.text()); }
